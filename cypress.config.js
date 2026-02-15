798caeb9963aa55524f95387a5899b8534297c66
Camilohafermann <camilohaf@gmail.com>
2026-02-15 20:22:48 -0300

    Criando pipeline do projeto no actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+     projectId: "ecdiyn",
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
+    
   },
 });
