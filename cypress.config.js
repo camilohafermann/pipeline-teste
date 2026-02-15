fc70c03a80a285318703c6f981c70a8a141361a4
Camilo Hafermann <camilohaf@gmail.com>
2026-02-15 20:28:57 -0300

    Revert "Criando pipeline do projeto no actions"

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,8 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-     projectId: "ecdiyn",
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
-    
   },
 });
